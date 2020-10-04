e7506da9d22247dfe5ea080afc5815ea2da761d8
Arcuman <anton.arcumn.borisov@gmail.com>
2020-10-04 14:16:25 +0300

    feat: add solution on first 5 task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+	let num = parseFloat(sampleActivity);
+	console.log("INPUT: " + sampleActivity + " NUM: " + num);
+	if (sampleActivity == null || typeof (sampleActivity) != "string" || !num || num < 0)
+		return false;
+
+	let k = 0.693 / HALF_LIFE_PERIOD;
+	t = Math.ceil(Math.log2(MODERN_ACTIVITY / sampleActivity) / k);
+	if (t < 0)
+		return false;
+	return t;
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+	let count = 0;
+	if (matrix == null)
+		return count;
+	let arr = matrix.flat(Infinity);
+	for (let item of arr) {
+		if (item == '^^')
+			count++;
+	}
+	return count;
 };
